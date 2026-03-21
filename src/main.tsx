b77e342d402f833ffc77c8816d39bb79b9b25690
Igor <contato.igorgabriel@hotmail.com>
2026-03-20 21:56:52 -0300

    🔨 refactor: remove non-null assertion and handle root element

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { APP_ROOT_ID } from "~constants";
 import { App } from "./App";
 
-createRoot(document.getElementById("root")!).render(
+if (!APP_ROOT_ID) throw new Error("Root element not found");
+
+createRoot(APP_ROOT_ID).render(
     <StrictMode>
         <App />
     </StrictMode>
