6c15adadbfd3ddefa6c21d08e2b75cff88866c51
Igor <contato.igorgabriel@hotmail.com>
2026-03-20 21:55:39 -0300

    🔧 chore: setup absolute imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
+import { resolve } from "node:path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    plugins: [react()]
+    plugins: [react()],
+    resolve: {
+        alias: {
+            "~": resolve(__dirname, "src/")
+        }
+    }
 });
